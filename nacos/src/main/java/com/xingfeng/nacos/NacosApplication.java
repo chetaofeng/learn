efac1eb0ac46d29ce34276c570cf2a1c35ef2226
chetaofeng <chetaofeng@1d1d100.com>
2019-05-31 18:33:52 +0800

    Initial commit

---
+++ nacos/src/main/java/com/xingfeng/nacos/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.xingfeng.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+}
